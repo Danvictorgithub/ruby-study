2b55e42bafd095068d2d78163ef0731806032716
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2023-06-04 12:18:08 +0800

    finished input and output chapter

---
+++ basic-ruby/3_input-and-output/input-and-output.rb
@@ -0,0 +1,3 @@
+puts "Hello World" #=> creates new line ("Hello World\n")
+printf ("Hello World") #=> doesn't create new line("Hello World")
+p " Hello Neighbor" #=> shorthand for puts also returns raw version of the object, and has a return value
