6d476fe25c45123fceff9ba21652d1146dbd01f8
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2023-06-04 11:22:50 +0800

    changed folder

---
+++ basic-ruby/2_variables/variables.rb
@@ -0,0 +1,19 @@
+#Declaring Variable
+
+age = 18 #=> 18
+age = 18 + 5 #=> 23
+
+age += 2 #=> 25
+age -= 2 #=> 23
+age /= 2 #=> 11
+age * 2 #=> 22
+
+desired_location = "Barcelona"
+johns_location = desired_location
+johns_location.upcase!
+
+desired_location
+johns_location
+
+name = gets.chomp
+puts name
