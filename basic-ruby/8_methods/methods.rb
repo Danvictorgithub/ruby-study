23719000f3a112fe3aa942e8ef3ebfbda6164f59
Dan Victor Lofranco <danvictorprogramming1@gmail.com>
2023-06-06 08:36:59 +0800

    finished methods chapter

---
+++ basic-ruby/8_methods/methods.rb
@@ -0,0 +1,42 @@
+# # def greet(name)
+# #   puts "Hello #{name}"
+# # end
+
+# # greet "Dan"
+# # def greet(name = "stranger")
+# #   puts "Hello #{name}"
+# # end
+
+# # greet
+
+# def even_odd(number)
+#   unless number.is_a? Numeric
+#     return "A number was not entered."
+#   end
+
+#   if number % 2 == 0
+#     "That is an even number."
+#   else
+#     "That is an odd number."
+#   end
+# end
+
+# puts even_odd(20) #=>  That is an even number.
+# puts even_odd("Ruby") #=>  A number was not entered.
+#
+# phrase = ["be", "to", "not", "or", "be", "to"]
+# puts phrase.reverse.join(" ").capitalize
+#
+# puts 12.between?(1,16)
+
+# whipser = "HELLO WORLD"
+
+# puts whipser.downcase
+# puts whipser
+
+def some_number (number)
+  number = 5
+end
+a = 1
+some_number a
+puts a
